6b008cdc1e7792d3f9b6d45f150318d4a5050028
Žiga Jerina <ziga.jerina@gmail.com>
2025-02-09 19:39:44 +0100

    perf: improve performance for large scoreboards

--- src/scoreboard.ts
+++ src/scoreboard.ts
@@ -54,6 +54,17 @@ class Match {
     return this.homeTeam.score + this.awayTeam.score;
   }
 
+  public getHash(): string {
+    return `${this.homeTeam.name}_${this.awayTeam.name}`;
+  }
+
+  public static getHashFromNames(
+    homeTeamName: string,
+    awayTeamName: string,
+  ): string {
+    return `${homeTeamName}_${awayTeamName}`;
+  }
+
   public toString(): string {
     return `sequenceNum=${this.sequenceNum} homeTeamName=${this.homeTeam.name} homeTeamScore=${this.homeTeam.score} awayTeamName=${this.awayTeam.name} awayTeamScore=${this.awayTeam.score}`;
   }
@@ -62,12 +73,14 @@ class Match {
 export class Scoreboard {
   private sequenceCounter: number;
   private teamToMatchMapping: Record<string, Match>;
+  private matchIndexMapping: Record<string, number>;
   private matches: Match[];
   private readonly MAX_TEAM_NAME_LEN = 1024;
 
   constructor() {
     this.sequenceCounter = 0;
     this.teamToMatchMapping = {};
+    this.matchIndexMapping = {};
     this.matches = [];
   }
 
@@ -77,7 +90,9 @@ export class Scoreboard {
     const match = new Match(this.sequenceCounter++, homeTeamName, awayTeamName);
     this.teamToMatchMapping[homeTeamName] = match;
     this.teamToMatchMapping[awayTeamName] = match;
+    this.matchIndexMapping[match.getHash()] = this.matches.length;
     this.matches.push(match);
+    this.bubbleUpMatch(this.matches.length - 1);
     return match;
   }
 
@@ -87,58 +102,83 @@ export class Scoreboard {
     homeTeamScore: number,
     awayTeamScore: number,
   ) {
-    const match = this.matches.find(
-      (match) =>
-        match.homeTeam.name === homeTeamName &&
-        match.awayTeam.name === awayTeamName,
-    );
-    if (!match) {
+    const index =
+      this.matchIndexMapping[
+      Match.getHashFromNames(homeTeamName, awayTeamName)
+      ];
+    if (index === undefined) {
       throw new Error(
         `Match between homeTeam=${homeTeamName} and awayTeam=${awayTeamName} does not exist on the scoreboard`,
       );
     }
 
+    const match = this.matches[index];
     this.validateScore(homeTeamScore, match.homeTeam.score, "home team");
     this.validateScore(awayTeamScore, match.awayTeam.score, "away team");
     match.homeTeam.score = homeTeamScore;
     match.awayTeam.score = awayTeamScore;
+    this.bubbleUpMatch(index);
   }
 
   public finishMatch(homeTeamName: string, awayTeamName: string) {
-    const index = this.matches.findIndex(
-      (match) =>
-        match.homeTeam.name === homeTeamName &&
-        match.awayTeam.name === awayTeamName,
-    );
-    if (index < 0) {
+    const index =
+      this.matchIndexMapping[
+      Match.getHashFromNames(homeTeamName, awayTeamName)
+      ];
+    if (index === undefined) {
       throw new Error(
         `Match between homeTeam=${homeTeamName} and awayTeam=${awayTeamName} does not exist on the scoreboard`,
       );
     }
 
+    const match = this.matches[index];
     delete this.teamToMatchMapping[homeTeamName];
     delete this.teamToMatchMapping[awayTeamName];
+    delete this.matchIndexMapping[match.getHash()];
+    for (let i = index + 1; i < this.matches.length; i++) {
+      const followingMatch = this.matches[i];
+      this.matchIndexMapping[followingMatch.getHash()]--;
+    }
     this.matches.splice(index, 1);
   }
 
-  public getSummary(): MatchInfo[] {
-    this.matches.sort((a, b) => {
-      if (a.getTotalScore() > b.getTotalScore()) {
-        return -1;
-      } else if (a.getTotalScore() < b.getTotalScore()) {
-        return 1;
+  private bubbleUpMatch(matchIndex: number) {
+    if (matchIndex === 0) return;
+    const match = this.matches[matchIndex];
+    for (let i = matchIndex - 1; i >= 0; i--) {
+      const nextMatch = this.matches[i];
+      if (this.compareMatches(match, this.matches[i]) === -1) {
+        this.matches[i + 1] = nextMatch;
+        this.matchIndexMapping[nextMatch.getHash()]++;
+      } else {
+        this.matches[i + 1] = match;
+        this.matchIndexMapping[match.getHash()] = i + 1;
+        return;
       }
-      if (a.sequenceNum > b.sequenceNum) {
-        return -1;
-      }
-      if (a.sequenceNum < b.sequenceNum) {
-        return 1;
-      }
-      throw new Error(
-        `Unexpected scoreboard state matchA=(${a.toString()}), matchB=(${b.toString()})`,
-      );
-    });
+    }
 
+    this.matchIndexMapping[match.getHash()] = 0;
+    this.matches[0] = match;
+  }
+
+  private compareMatches(a: Match, b: Match): number {
+    if (a.getTotalScore() > b.getTotalScore()) {
+      return -1;
+    } else if (a.getTotalScore() < b.getTotalScore()) {
+      return 1;
+    }
+    if (a.sequenceNum > b.sequenceNum) {
+      return -1;
+    }
+    if (a.sequenceNum < b.sequenceNum) {
+      return 1;
+    }
+    throw new Error(
+      `Unexpected scoreboard state matchA=(${a.toString()}), matchB=(${b.toString()})`,
+    );
+  }
+
+  public getSummary(): MatchInfo[] {
     return this.matches.map((match) => ({
       homeTeamName: match.homeTeam.name,
       homeTeamScore: match.homeTeam.score,
